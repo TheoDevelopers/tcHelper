d611dca4466968edbf1205a9b9f6f951c2f05eff
Joel Montes de Oca <JoelMontes01@gmail.com>
2019-04-03 23:34:20 -0400

    Added mock to conf.py so RTD can work

--- docs/conf.py
+++ docs/conf.py
@@ -52,3 +52,13 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['PySide2]
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
