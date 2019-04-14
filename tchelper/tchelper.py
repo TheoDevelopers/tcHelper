4f6c193e9ed5f5acb13ffec0826add010047f47d
Joel Montes de Oca <JoelMontes01@gmail.com>
2019-04-13 21:08:01 -0400

    Refactored configr.Configr + more
    Did a few more things such as hooked up the boot to the initdb so that
    the database can be init using the new database module.

--- tchelper/tchelper.py
+++ tchelper/tchelper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.7
 from PySide2 import QtWidgets
-from .configr import Configr
+from configr import Configr
+from database import InitDB
 
 
 def main():
@@ -8,7 +9,7 @@ def main():
     Entry point for the tcHelper program.
     """
     config_file = Configr()
-    if config_file.get_value('APP', 'first_time_running') == 'TRUE':
+    if config_file.getValue('APP', 'first_time_running') == 'True':
 
         QtWidgets.QApplication()
         file_name = QtWidgets.QFileDialog.getSaveFileName(None, "Save New Database", "New_Database.tcd",
@@ -18,10 +19,15 @@ def main():
             print('Please run tcHelper again and select a location to save the database')
             quit()
 
-        config_file.change_value('DB', 'location', file_name[0])
-        config_file.change_value('APP', 'first_time_running', 'FALSE')
+        config_file.setValue('DB', 'location', file_name[0])
+        config_file.setValue('APP', 'first_time_running', 'False')
 
-        DB.db_init()  # Initialize a new database
+        db = InitDB()
+        db.setDB(file_name[0])
+
+
+
+
 
     else:
         print('RUN GUI')
