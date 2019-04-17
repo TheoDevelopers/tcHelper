efe0d4c84197b20b80d8d37c9763294d949005a9
Joel Montes de Oca <JoelMontes01@gmail.com>
2019-04-16 22:48:16 -0400

    The import test were invalid
    I do not think I was testing for imported modules correctly so I removed 
    the tests from the test module.

--- test/test_database.py
+++ test/test_database.py
@@ -3,15 +3,4 @@ from tchelper.database import DB
 from tchelper.config import Config
 
 
-def test_imports():
-    """
-    Test to see modules can be imported from the module being tested.
-
-    .. warning:: I am not sure if this is a vaild test. There must be a better way of doing this.
-    """
-
-    try:
-        import sqlalchemy
-        import config
-    except ModuleNotFoundError as e:
-        pytest.fail(e, pytrace=True)
+pass
