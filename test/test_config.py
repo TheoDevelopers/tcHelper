a945414d6396eaccb42281ff5431c9806768698f
Joel Montes de Oca <JoelMontes01@gmail.com>
2019-04-16 21:47:14 -0400

    Changing the script call so pytest can find all modules
    PyTest was having a hard time finding my modules because it was being 
    runed from outside of the working dir. Added `cd tchelper && python -m 
    pytest ../test/ -v` to see if that would fix the issue.

--- test/test_config.py
+++ test/test_config.py
@@ -7,6 +7,21 @@ from unittest import mock
 import pytest
 
 
+def test_imports():
+    """
+    Test to see modules can be imported from the module being tested.
+
+    .. warning:: I am not sure if this is a vaild test. There must be a better way of doing this.
+    """
+
+    try:
+        from tchelper.config import configparser
+        from tchelper.config import os
+        #        from tchelper.config import os.path  # Doesn't want to work.
+    except ModuleNotFoundError as e:
+        pytest.fail(e, pytrace=True)
+
+
 @mock.patch('tchelper.config.os.path.isfile')
 @mock.patch('tchelper.config.os.access')
 @pytest.mark.parametrize('isfile, access, result', [(True, True, True),
